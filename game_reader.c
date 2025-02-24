4c6ac0fae42191757743e6a9346942480825087f
DPR206 <duna.puente@estudiante.uam.es>
2025-02-24 10:37:27 +0100

    Update game_reader.c

--- game_reader.c
+++ game_reader.c
@@ -4,41 +4,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-/**
- * @brief It loads game spaces from a file
- * @author Duna Puente
- *
- * @param game Pointer to the game
- * @param filename Name of the file containing space data
- * @return Status of the operation: ERROR if failed, OK if successful.
- */
-Status game_load_spaces(Game *game, char *filename);
-
-/**
- * @brief It adds a space to the games space array
- * @author Profesores PPROG
- * 
- * @param game Pointer to the game
- * @param space Pointer to the space to add
- * @return Status of the operation: ERROR if failes, OK if successful 
- */
-Status game_add_space(Game *game, Space *space);
-
-/**
- * @brief It gets the space ID at a specific position
- * @author Profesores PPROG
- * 
- * @param game Pointer to the game
- * @param position Position to get its space ID 
- * @return The ID at the specific position, or NO_ID if the position is not valid
- */
-Id game_get_space_id_at(Game *game, int position);
-
-/**
-   Implementation of private functions
-*/
-
-Status game_load_spaces(Game *game, char *filename)
+Status game_reader_load_spaces(Game *game, char *filename)
 {
   FILE *file = NULL;
   char line[WORD_SIZE] = "";
@@ -99,43 +65,4 @@ Status game_load_spaces(Game *game, char *filename)
   fclose(file);
 
   return status;
-}
-
-Status game_add_space(Game *game, Space *space)
-{
-  if ((space == NULL) || (game->n_spaces >= MAX_SPACES))
-  {
-    return ERROR;
-  }
-
-  game->spaces[game->n_spaces] = space;
-  game->n_spaces++;
-
-  return OK;
-}
-
-Id game_get_space_id_at(Game *game, int position)
-{
-  if (position < 0 || position >= game->n_spaces)
-  {
-    return NO_ID;
-  }
-
-  return space_get_id(game->spaces[position]);
-}
-
-Status game_create_from_file(Game *game, char *filename) {
-  if (game_create(game) == ERROR) {
-    return ERROR;
-  }
-
-  if (game_load_spaces(game, filename) == ERROR) {
-    return ERROR;
-  }
-
-  /* The player and the object are located in the first space */
-  game_set_player_location(game, game_get_space_id_at(game, 0));
-  game_set_object_location(game, game_get_space_id_at(game, 0));
-  
-  return OK;
 }
