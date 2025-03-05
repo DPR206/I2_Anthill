b7610bafd83cfdbdb099250b396a6e5c51327c97
claudsze <clasaiz@outlook.es>
2025-03-05 12:55:43 +0100

    Add files via upload

--- graphic_engine.c
+++ graphic_engine.c
@@ -143,10 +143,10 @@ void graphic_engine_paint_game(Graphic_engine *ge, Game *game)
 
   /* Paint in the description area */
   screen_area_clear(ge->descript);
-  grain_loc = game_get_object_location_from_name; /*Apaño de obj_id, deberia ser el id de cada uno de los objetos*/
-  crumb_loc = game_get_object_location(game, obj_id);
-  leaf_loc = game_get_object_location(game, obj_id);
-  seed_loc = game_get_object_location(game, obj_id);
+  grain_loc = game_get_object_location_from_name(game, "Grain");
+  crumb_loc = game_get_object_location_from_name(game, "Crumb");
+  leaf_loc = game_get_object_location_from_name(game, "Leaf");
+  seed_loc = game_get_object_location_from_name(game, "Seed");
   if ((grain_loc!= NO_ID)||(crumb_loc!= NO_ID)||(leaf_loc!= NO_ID)||(seed_loc!= NO_ID))
   {
     sprintf(str, "Objects:\n  Grain:%d\n  Crumb:%d\n  Leaf:%d\n  Seed:%d\n", (int)grain_loc, (int)crumb_loc, (int)leaf_loc, (int)seed_loc);
