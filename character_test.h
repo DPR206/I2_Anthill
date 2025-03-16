f100624993ab351554bf2441e0df11ad1816cbbb
DPR206 <duna.puente@estudiante.uam.es>
2025-03-16 16:11:58 +0100

    Add files via upload

---
+++ character_test.h
@@ -0,0 +1,56 @@
+/** 
+ * @brief It declares the tests for the character module
+ * 
+ * @file character_test.h
+ * @author Duna Puente
+ * @version 1 
+ * @date 13-03-2025
+ * @copyright GNU Public License
+ */
+
+#ifndef TEST_CHARACTER_H
+#define TEST_CHARACTER_H
+
+void test1_character_create();
+void test2_character_create();
+
+void test1_character_destroy();
+void test2_character_destroy();
+
+void test1_character_setid();
+void test2_character_setid();
+
+void test1_character_get_id();
+void test2_character_get_id();
+
+void test1_character_set_name();
+void test2_character_set_name();
+
+void test1_character_get_name();
+void test2_character_get_name();
+
+void test1_character_set_gdesc();
+void test2_character_set_gdesc();
+
+void test1_character_get_gdesc();
+void test2_character_get_gdesc();
+
+void test1_character_set_health();
+void test2_character_set_health();
+
+void test1_character_get_health();
+void test2_character_get_health();
+
+void test1_character_set_friendly();
+void test2_character_set_friendly();
+
+void test1_character_get_friendly();
+void test2_character_get_friendly();
+
+void test1_character_set_message();
+void test2_character_set_message();
+
+void test1_character_get_message();
+void test2_character_get_message();
+
+#endif
