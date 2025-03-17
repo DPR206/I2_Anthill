2debefa461178588e2e1262f6098dfb8eddabbd3
DPR206 <duna.puente@estudiante.uam.es>
2025-03-17 19:36:46 +0100

    Add files via upload

--- space_test.c
+++ space_test.c
@@ -13,10 +13,11 @@
 #include <string.h> 
 #include "space.h"
 #include "set.h"
+#include "character.h"
 #include "space_test.h"
 #include "test.h"
 
-#define MAX_TESTS 44
+#define MAX_TESTS 48
 
 /** 
  * @brief Main function for SPACE unit tests. 
@@ -85,10 +86,14 @@ int main(int argc, char** argv) {
   if (all || test == 38) test1_space_at_object();
   if (all || test == 39) test2_space_at_object();
   if (all || test == 40) test3_space_at_object();
-  if (all || test == 41) test1_space_set_gdesc();
-  if (all || test == 42) test2_space_set_gdesc();
-  if (all || test == 43) test1_space_get_gdesc();
-  if (all || test == 44) test2_space_get_gdesc();
+  if (all || test == 41) test1_space_set_character();
+  if (all || test == 42) test2_space_set_character();
+  if (all || test == 43) test1_space_get_character();
+  if (all || test == 44) test2_space_get_character();
+  if (all || test == 45) test1_space_set_gdesc();
+  if (all || test == 46) test2_space_set_gdesc();
+  if (all || test == 47) test1_space_get_gdesc();
+  if (all || test == 48) test2_space_get_gdesc();
 
 
 
@@ -364,5 +369,75 @@ void test3_space_at_object(){
   s=space_create(20);
   space_set_object(s, id);
   PRINT_TEST_RESULT(space_at_object(s, id)==TRUE);
+  space_destroy(s);
+}
+
+void test1_space_set_character()
+{
+  Space *s = NULL;
+  Character *character = character_create();
+  PRINT_TEST_RESULT(space_set_character(s, character) == ERROR);
+  character_destroy(character);
+}
+
+void test2_space_set_character()
+{
+  Space *s = NULL;
+  Id id = 25;
+  Character *character = character_create();
+  s = space_create(id);
+  PRINT_TEST_RESULT(space_set_character(s, character) == OK);
+  character_destroy(character);
+  space_destroy(s);
+}
+
+void test1_space_get_character()
+{
+  Space *s = NULL;
+  PRINT_TEST_RESULT(space_get_character(s) == NULL);
+}
+
+void test2_space_get_character()
+{
+  Space *s = NULL;
+  Id id = 25;
+  Character *character = character_create();
+  s = space_create(id);
+  space_set_character(s, character);
+  PRINT_TEST_RESULT(space_get_character(s) != NULL);
+  space_destroy(s);
+}
+
+void test1_space_set_gdesc()
+{
+  Space *s = NULL;
+  char gdesc[GD_HEIGHT][GD_WIDTH+1]={"test"};
+  PRINT_TEST_RESULT(space_set_gdesc(s, gdesc) == ERROR);
+}
+
+void test2_space_set_gdesc()
+{
+  Space *s = NULL;
+  char gdesc[GD_HEIGHT][GD_WIDTH+1]={"test"};
+  Id id = 25;
+  s = space_create(id);
+  PRINT_TEST_RESULT(space_set_gdesc(s, gdesc) == OK);
+  space_destroy(s);
+}
+
+void test1_space_get_gdesc()
+{
+  Space *s = NULL;
+  int i = 1;
+  PRINT_TEST_RESULT(space_get_gdesc(s, i) == NULL);
+}
+
+void test2_space_get_gdesc()
+{
+  Space *s = NULL;
+  int i = 1;
+  Id id = 25;
+  s = space_create(id);
+  PRINT_TEST_RESULT(space_get_gdesc(s, i) != NULL);
   space_destroy(s);
 }
